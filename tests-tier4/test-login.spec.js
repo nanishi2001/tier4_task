1f9ba8132fa32784b828a4347290fa8b78b85a31
nanishi2001 <nanishi2001@gmail.com>
2024-04-26 13:08:51 +0900

    空入力時のバリデーションテストを追加

--- tests-tier4/test-login.spec.js
+++ tests-tier4/test-login.spec.js
@@ -6,7 +6,7 @@ const loginValue = readJSON(path);
 
 test.beforeEach(async ({page}) => {
 	await page.goto('https://account.tier4.jp/login');
-})
+});
 
 test('login', async ({ page }) => {
 	await page.locator('//*[@id="identifier"]').fill(loginValue.email);
@@ -16,5 +16,21 @@ test('login', async ({ page }) => {
 	await page.locator('//*[@id="password"]').fill(loginValue.password);
 	await page.getByRole('button', { name: 'Login' }).click();
 
-	await expect(page).toHaveURL("https://portal.tier4.jp/")
+	await expect(page).toHaveURL("https://portal.tier4.jp/");
 });
+
+test('Not entered identifier', async ({ page }) => {
+	await page.getByRole('button', { name: 'Login' }).click();
+
+	await expect(
+		page.locator('//*[@id="root"]/section/main/section/form/div[1]/label/span')
+	).toContainText('identifier is missing.')
+})
+
+test('Not entered password', async({ page }) => {
+	await page.getByRole('button', { name: 'Login' }).click();
+
+	await expect(
+		page.locator('//*[@id="root"]/section/main/section/form/div[2]/label/span')
+	).toContainText('password is missing.')
+})
